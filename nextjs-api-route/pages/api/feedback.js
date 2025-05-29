dac11c74707f584a53e85a7fee2c56a2d4bd4f97
HyerimKimm <50258232+HyerimKimm@users.noreply.github.com>
2025-05-30 01:32:57 +0900

    Feat. get 요청 시 조회한 데이터를 주기

--- nextjs-api-route/pages/api/feedback.js
+++ nextjs-api-route/pages/api/feedback.js
@@ -1,6 +1,16 @@
 import fs from "fs";
 import path from "path";
 
+function buildFeedbackPath() {
+  return path.join(process.cwd(), "data", "feedback.json"); // cwd : current working directory
+}
+
+function extractFeedback(filePath) {
+  const fileData = fs.readFileSync(filePath);
+  const data = JSON.parse(fileData);
+  return data;
+}
+
 function handler(req, res) {
   if (req.method === "POST") {
     const email = req.body.email;
@@ -13,15 +23,17 @@ function handler(req, res) {
     };
 
     // store that in a database or in a file
-    const filePath = path.join(process.cwd(), "data", "feedback.json"); // cwd : current working directory
-    const fileData = fs.readFileSync(filePath);
+    const filePath = buildFeedbackPath(); // cwd : current working directory
+    const fileData = extractFeedback(filePath);
     const data = JSON.parse(fileData);
     data.push(newFeedback);
     fs.writeFileSync(filePath, JSON.stringify(data));
     res.status(201).json({ message: "Success!", feedback: newFeedback });
   } else {
+    const filePath = buildFeedbackPath();
+    const data = extractFeedback(filePath);
     res.status(200).json({
-      message: "성공",
+      feedback: data,
     });
   }
 }
