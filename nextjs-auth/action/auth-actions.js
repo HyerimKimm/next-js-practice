8a7b0827659df1f5a2d79af467c0d207fd58f50f
HyerimKimm <50258232+HyerimKimm@users.noreply.github.com>
2025-08-07 23:15:29 +0900

    Feat. sqlite에 회원 데이터 저장 + 비밀번호 암호화

--- nextjs-auth/action/auth-actions.js
+++ nextjs-auth/action/auth-actions.js
@@ -1,5 +1,8 @@
 "use server";
 
+import { hashUserPassword } from "@/lib/hash";
+import { createUser } from "@/lib/user";
+
 export async function signup(prevState, formData) {
   const email = formData.get("email");
   const password = formData.get("password");
@@ -10,11 +13,14 @@ export async function signup(prevState, formData) {
     errors.email = "Please enter a valid email address";
   }
 
-  if (!password.trim().length < 8) {
+  if (password.trim().length < 8) {
+    console.log(password.trim().length);
     errors.password = "Password must be at least 8 characters long";
   }
 
   if (Object.keys(errors).length > 0) {
-    return { errors };
+    return { errors: errors };
   }
+  const hashedPassword = hashUserPassword(password);
+  createUser(email, hashedPassword);
 }
